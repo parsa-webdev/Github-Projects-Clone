123a1949c3c252457a0456984b59f1a83fd866eb
parsa-webdev <60968775+parsa-webdev@users.noreply.github.com>
2020-10-04 12:26:13 +0600

    Router and Vuetify Setup

---
+++ client/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <Header />
+    <router-view></router-view>
+  </v-app>
+</template>
+
+<script>
+import Header from "./components/Header";
+export default {
+  name: "App",
+  components: {
+    Header,
+  },
+};
+</script>
---
+++ client/src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <router-link to="/">home</router-link>
+    <router-link to="/register">register</router-link>
+    <router-link to="/login">login</router-link>
+    <router-link to="/dashboard">dashboard</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style></style>
