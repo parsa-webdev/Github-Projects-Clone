89de8c6ff07b4b537c5a11cc08fb49c5958045e3
parsa-webdev <60968775+parsa-webdev@users.noreply.github.com>
2020-10-07 20:17:55 +0600

    Backend Connected To Frontend

--- client/src/store/modules/auth.js
+++ client/src/store/modules/auth.js
@@ -4,23 +4,37 @@ const state = {
   user: null,
   isLoggedIn: false,
   authenticating: false,
-  verifyingUser: true,
+  verifyingUser: false,
   errors: null,
 };
-const getters = {};
+const getters = {
+  isLoggedIn: (state) => state.isLoggedIn,
+  authenticating: (state) => state.authenticating,
+  errors: (state) => state.errors,
+  verifyingUser: (state) => state.verifyingUser,
+  user: (state) => state.user,
+};
 const actions = {
   async login({ commit }, userInput) {
     try {
-      console.log(userInput);
+      commit("authenticating", null);
+
+      const res = await axios.post("auth/login", userInput);
+
+      commit("authenticated", res.data);
     } catch (err) {
-      console.log(err.response.data);
+      commit("setErrors", { login: err.response.data.error.message });
     }
   },
   async register({ commit }, userInput) {
     try {
-      console.log(userInput);
+      commit("authenticating", null);
+
+      const res = await axios.post("auth/register", userInput);
+
+      commit("authenticated", res.data);
     } catch (err) {
-      console.log(err);
+      commit("setErrors", { register: err.response.data.error.message });
     }
   },
   logout({ commit }) {
@@ -29,6 +43,25 @@ const actions = {
   },
 };
 const mutations = {
+  verifyingUser: (state, value) => {
+    state.verifyingUser = true;
+    state.errors = null;
+  },
+  authenticated: (state, user) => {
+    state.isLoggedIn = true;
+    state.authenticating = false;
+    state.verifyingUser = false;
+    state.errors = null;
+    state.user = user;
+  },
+  authenticating: (state, value) => {
+    state.authenticating = true;
+  },
+  setErrors: (state, err) => {
+    state.errors = err;
+    state.authenticating = false;
+    state.verifyingUser = false;
+  },
   logout: (state, value) => {
     state.authenticating = false;
     state.isLoggedIn = false;
