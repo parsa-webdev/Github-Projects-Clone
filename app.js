f1ab89d90cfa60e6ad14a48a25ff7ab348b9346c
parsa-webdev <60968775+parsa-webdev@users.noreply.github.com>
2020-12-07 12:50:13 +0600

    testing deploy

--- app.js
+++ app.js
@@ -11,19 +11,19 @@ dotenv.config();
 
 const app = express();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("/public"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "/", "public", "index.html"));
-  });
-}
-
 app.use(morgan("dev"));
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.get("/", async (req, res, next) => {
   res.send("Express server");
