56007501ca5209cc6542d0850af6d371ac5ce9f7
parsa-webdev <60968775+parsa-webdev@users.noreply.github.com>
2020-12-10 19:59:50 +0600

    testing deploy

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(__dirname + "/public/"));
 
-  app.get(/.*/, (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "public", "index.html"));
   });
 }
